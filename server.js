b0cf68e5ae79fd78dcebfabc8e91cfacd50997a3
Saied Gaffer <saied2421998@gmail.com>
2021-08-06 14:30:49 +0200

    editing

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const nodemailer = require("nodemailer")
 const cors = require('cors');
 const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
@@ -17,6 +18,63 @@ const app1 = new Clarifai.App({
     ssl : false
   }
 });
+
+const smtpTransport = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+        user: "saied2421998@gmail.com",
+        pass: "saied1998"
+    }
+});
+let rand,mailOptions,host,link;
+
+
+//start
+
+app.get('/send',function(req,res){
+        rand=Math.floor((Math.random() * 100) + 54);
+    host=req.get('host');
+    link="http://"+req.get('host')+"/verify?id="+rand;
+    mailOptions={
+        to : req.query.to,
+        subject : "Please confirm your Email account",
+        html : "Hello,Please Click on the link to verify your email."+link+">Click here to verify"
+    }
+    console.log(mailOptions);
+    smtpTransport.sendMail(mailOptions, function(error, response){
+     if(error){
+            console.log(error);
+        res.end("error");
+     }else{
+            console.log("Message sent: " + response.message);
+        res.end("sent");
+         }
+});
+});
+
+app.get('/verify',function(req,res){
+console.log(req.protocol+":/"+req.get('host'));
+if((req.protocol+"://"+req.get('host'))==("http://"+host))
+{
+    console.log("Domain is matched. Information is from Authentic email");
+    if(req.query.id==rand)
+    {
+        console.log("email is verified");
+        res.end("Email "+mailOptions.to+" is been Successfully verified");
+    }
+    else
+    {
+        console.log("email is not verified");
+        res.end("Bad Request");
+    }
+}
+else
+{
+    res.end("Request is from unknown source");
+}
+});
+
+//end
 const app=express();
 
 app.use(cors());
