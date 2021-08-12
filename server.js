470470591caaa05311b0ef9476d63843e21f7ee9
Saied Gaffer <saied2421998@gmail.com>
2021-08-12 14:29:25 +0200

    test dataase

--- server.js
+++ server.js
@@ -13,9 +13,10 @@ const app1 = new Clarifai.App({
  const db = knex({
   client: 'pg',
   connection: {
-    connectionString : process.env.DATABASE_URL,
-    ssl : false
-  }
+	  connectionString: process.env.DATABASE_URL,
+	  ssl: {
+	    rejectUnauthorized: false
+	  }
 });
 
 
