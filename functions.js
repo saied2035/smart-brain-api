7ae305c1e710b21be53151d6a20e73bd1328f710
Saied Gaffer <saied2421998@gmail.com>
2021-08-25 16:02:09 +0200

    check pass

--- functions.js
+++ functions.js
@@ -21,7 +21,7 @@ const smtpTransport = nodemailer.createTransport({
 
 const checkPass = async (pass) => {
                data = await db('login').select('hash')
-               return Promise.resolve(data)
+               return pass(data)
 }
 
 
