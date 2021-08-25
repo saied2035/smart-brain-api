2e943120cfb3b5252231eb4ddb120f9bbb474ae6
Saied Gaffer <saied2421998@gmail.com>
2021-08-25 16:31:33 +0200

    check pass

--- server.js
+++ server.js
@@ -141,7 +141,8 @@ app.post('/predict',(req,res)  => {
       res.json('success')
 })*/
 app.get('/test',(req,res) => {
-     checkPass('saied1998').then((data) => console.log(data))
+     const data= checkPass('saied1998').then((data) => data)
+     console.log(data)
       res.json('success')
 /*   const isValid = checkPass('saied1998')
    console.log(isValid)
