1a31e37ded6ef0ff22cf2e72089d63491d97f7c5
Saied Gaffer <saied2421998@gmail.com>
2021-08-31 21:57:46 +0200

    edit email validation

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get('/',(req,res)  => {
 app.get('/send',(req,res) => {
     const code =codeGenerator();
     const mailOptions={
-        from: "smartbrain <saied2421998@gmail.com>",
+        from: "smartbrain <>",
         to : req.body.email,
         subject : "Please confirm your Email account",
         html : `<div style="text-align:center;font-size:20px;font-weight:600;">
