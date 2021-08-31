c118e044e86c1d2daa66cb373bfc53319e74814b
Saied Gaffer <saied2421998@gmail.com>
2021-08-31 21:51:09 +0200

    edit email validation

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get('/send',(req,res) => {
         from: "smartbrain <saied2421998@gmail.com>",
         to : req.body.email,
         subject : "Please confirm your Email account",
-        html : `<div style="text-align:center;font-size: 1.25rem;font-weight:600;">
+        html : `<div style="text-align:center;font-size:20px;font-weight:600;">
         <p style="margin-block:0;margin-block-end: 0.5rem;">
           Hello,Please enter this code in confirmation page :
         </p>
