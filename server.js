8509799dabb580e2707aff3d37b69a37221060c0
Saied Gaffer <saied2421998@gmail.com>
2021-09-08 19:36:15 +0200

    check image if receviced

--- server.js
+++ server.js
@@ -168,7 +168,7 @@ app.post('/predict',(req,res)  => {
               res.json(data)
             })
             .catch(error => {
-              console.log('error',error)
+              console.log('errorHandlingrequestsaied',error)
               res.status(400).json(error)
             })
 })
