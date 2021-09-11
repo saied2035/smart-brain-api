02fd33e8a786f7f0378fea6d62c21809f0130e0f
Saied Gaffer <saied2421998@gmail.com>
2021-09-11 16:17:34 +0200

    test face-api

--- server.js
+++ server.js
@@ -175,7 +175,14 @@ app.post('/predict',async (req,res) => {
               console.log('errorHandlingrequestsaied',error)
               res.status(400).json(error)
             })*/
-            const image = await loadImage(req.body.text)
+            let image
+            if(req.body.text.includes('http') || req.body.text.includes('https')){
+              image = await loadImage(req.body.text)
+            }
+            else{
+               const request = Buffer.from(req.body.image,"base64")
+               image = await loadImage(request)
+            }
             console.log("image",image)
             const detection = await faceapi.detectSingleFace(image)
             console.log("result",detection)
